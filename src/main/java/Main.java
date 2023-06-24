245f00651987e3863e508d09d1988e9e9a674f10
RolledBread <krzysztof.czajkowski43@gmail.com>
2023-06-24 13:57:33 +0200

    Commit 2 - bookshelf dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import library.Book;
 import library.BookShelf;
-import utility.FileIO;
-
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
         Book.loadBooks();
-        BookShelf bookShelf = new BookShelf(349,Book.listOfBooks);
+        BookShelf bookShelf = new BookShelf(200,Book.listOfBooks);
+        bookShelf.listAllBooks();
     }
 }
--- src/main/java/library/BookShelf.java
+++ src/main/java/library/BookShelf.java
@@ -3,12 +3,32 @@ package library;
 import java.util.ArrayList;
 
 public class BookShelf {
-    private int capacity;
-    private ArrayList<Book> allBooks;
+    int capacity;
+    ArrayList<Book> allBooks;
 
     public BookShelf(int capacity, ArrayList<Book> books){
         this.capacity = capacity;
         this.allBooks = books;
+    }
+    /** returns fill ration of bookshelf */
+    public String fillRatio(){
+        float amountOfBooks = allBooks.size();
+        float fillRatio = amountOfBooks/capacity;
+        int percentFillRatio = Math.round(fillRatio*100);
+
+        return percentFillRatio + "%";
+    }
+    /**returns number of books and fill ratio of the shelf*/
+    public String shelfStatus(){
+        return "There are " + allBooks.size() + " books on the shelf.\nFill status: "+ fillRatio();
+    }
+    /**Prints the list of all books on the shelf*/
+    public void listAllBooks(){
+        int index = 1;
+        for(Book book:allBooks){
+            System.out.println(index +" "+ book);
+            index++;
+        }
     }
 
 }
